082c7d7d1aebd2d42c3ef4746e6e732348d68b75
michael thomas <michael.thomas2014@gmail.com>
2020-06-08 00:37:26 +0000

    Automatically backed up by Learn

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -18,7 +18,7 @@ class Pokemon
  
   
   def self.find(db, id)
-    db.execute("SELECT * FROM pokemon WHERE id = ?", id).each do |row|
+    db.execute("SELECT * FROM pokemon WHERE id = (?)", id).each do |row|
       pokemon = Pokemon.new(row[0], row[1], row[2])
     end
   end
