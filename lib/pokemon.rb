a47e3652cb884cb0a8caf0f99b8682c886ee486b
michael thomas <michael.thomas2014@gmail.com>
2020-06-08 00:26:46 +0000

    Automatically backed up by Learn

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -1,2 +1,29 @@
 class Pokemon
+  attr_accessor :name, :type, :db, :id
+  
+  
+  def initialize(id: , name: , type: , db: )
+    @name = name
+    @type = type
+    @db = db
+    @id = id
+  end
+  
+  def self.save(name, type, db)
+    new = Pokemon.new
+    sql = <<-SQL
+      INSERT INTO songs (name, type, db)
+      VALUES (?, ?, ?)
+    SQL
+ 
+    DB[:conn].execute(sql, new.name, new.type, new.db)
+    @id = DB[:conn].execute("SELECT last_insert_rowid() FROM pokemon ")[0][0]
+  end
+  
+  def self.find 
+    
+  
+  end
+  
+  
 end
